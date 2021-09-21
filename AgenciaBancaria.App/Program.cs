5062b24a2008e745f2e63d7ec782b5921c7d934a
Rafael C. Silva <rafael.silva623@fatec.sp.gov.br>
2021-09-21 08:52:51 -0300

    Initial Commit

---
+++ AgenciaBancaria.App/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using AgenciaBancaria.Dominio;
+
+namespace AgenciaBancaria.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Cliente cliente = new Cliente("Carla",
+                                        "123.456.789-10",
+                                        "18822822-65",
+                                        "Alzira Bezera",
+                                        "25105195",
+                                        "Sorocaba",
+                                        "São Paulo");
+        }
+    }
+}
---
+++ AgenciaBancaria.Dominio/Cliente.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace AgenciaBancaria.Dominio
+{
+    public class Cliente
+    {
+        public Cliente(string nome,string cpf, string rg, string logradouro, string cep, string cidade, string Estado)
+        {
+            Nome = ValidaStringVazia(nome);
+            CPF = ValidaStringVazia(cpf);
+            RG = ValidaStringVazia(rg);
+            Logradouro = ValidaStringVazia(logradouro);
+            CEP = ValidaStringVazia(cep);
+            Cidade = ValidaStringVazia(cidade);
+            Estado = ValidaStringVazia(Estado);
+        }
+
+        private static string ValidaStringVazia(string texto)
+        {
+            return string.IsNullOrWhiteSpace(texto) ? throw new Exception("Propriedade deve estar preenchida") : texto;
+        }
+
+        public string Nome { get; private set; }
+        public string CPF { get; private set; } 
+        public string RG { get; private set; }  
+
+        public string Logradouro { get; private set; } 
+        public string CEP { get; private set; }
+        public string Cidade { get; private set; }
+        public string Estado  { get; private set; }
+    }
+
+}
